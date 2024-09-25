57ab51b1b38fea8c83dbddcc63fe8b5a888687b3
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-26 03:41:37 +0600

    var let and const difference

---
+++ var-let-const.js
@@ -0,0 +1,9 @@
+/**
+ * 1. var: now reason to use var
+ * 2. let: allow it to reassign
+ * 3. const: do not allow it to reassign
+ */
+
+const money = 25;
+const rich = money + 25
+console.log(rich);
