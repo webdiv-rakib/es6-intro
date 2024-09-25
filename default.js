12eb5424ce8876f8d9d64256aabedeb0b8738cfc
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-26 04:18:15 +0600

    explaining default value

---
+++ default.js
@@ -0,0 +1,15 @@
+// default --> if value is not provided, take this as default.
+function add(num1 = 0, num2 = 0) {
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+
+// const sum = add(5, 2);
+// const sum = add(5,7);
+const sum = add();
+
+function fullName(first, last) {
+    const full = first + " " + last;
+    console.log(full);
+}
