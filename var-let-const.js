20bc9060d5a9ddf3f630f70219b51c20dd27d5df
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-26 03:55:59 +0600

    var,let and const explanation

--- var-let-const.js
+++ var-let-const.js
@@ -7,3 +7,26 @@
 const money = 25;
 const rich = money + 25
 console.log(rich);
+
+let count = 0;
+count = count + 10;
+console.log(count);
+
+const numbers = [5, 7, 8, 9, 11, 15, 17];
+numbers[0] = 3;
+numbers.push(18, 22, 28);
+console.log(numbers);
+
+// object
+const student = {
+    name: 'Rakib',
+    class: '12'
+}
+student.name = 'Rakibul Hassan';
+console.log(student);
+
+let sum = 0
+for (let i = 0; i < 10; i++) {
+    sum = sum + i;
+}
+console.log(sum);
